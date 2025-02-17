8cdbaa8dbc22aba55f60188c4335ef4791af5f5b
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2025-02-17 15:25:09 +0800

    fix: made internal functions private in PhoneBook class and changed PhoneBook.cpp file structure to match order of functions.

--- 00/ex01/PhoneBook.cpp
+++ 00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 14:04:04 by etien             #+#    #+#             */
-/*   Updated: 2025/02/17 13:58:59 by etien            ###   ########.fr       */
+/*   Updated: 2025/02/17 15:18:26 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,89 +20,6 @@
 PhoneBook::PhoneBook() : _contactCount(0), _oldestIndex(0) {}
 
 PhoneBook::~PhoneBook() {}
-
-// This function stores a contact into the phonebook's contacts array and updates
-// the contactCount and oldestIndex variables.
-void PhoneBook::addContact(const Contact &newContact)
-{
-	if (_contactCount < 8)
-	{
-		_contacts[_contactCount] = newContact;
-		_contactCount++;
-	}
-	else
-	{
-		_contacts[_oldestIndex] = newContact;
-		// oldest index will be + 1, but % 8 to remain within the
-		// bounds of the array
-		_oldestIndex = (_oldestIndex + 1) % 8;
-		// contactCount doesn't have to be updated because arr is already
-		// at max contacts
-	}
-}
-
-// This function will truncate the text if it is has more than 10 characters.
-// The first 9 characters will be preserved and appended with a '.'.
-static std::string formatText(const std::string& text)
-{
-	if (text.length() > 10)
-		return text.substr(0, 9) + ".";
-	return text;
-}
-
-// This function displays all the contacts in the phonebook in a summarized form.
-// Workflow:
-// 1) Set the width of each column to 10 characters.
-// setw is a non-persistent manipulator (only applies to the next output) so it should
-// be called last. It will pad shorter text with space on the left (by default).
-// 2) Truncate the text if it exceeds 10 characters, replacing the last
-// character with a dot by calling formatText.
-void PhoneBook::displayContacts() const
-{
-	std::cout << "-----------------------------------------------------" << std::endl;
-	std::cout << "|    Index   | First Name |  Last Name |  Nickname  |" << std::endl;
-	std::cout << "-----------------------------------------------------" << std::endl;
-
-	for (int i = 0; i < _contactCount; i++)
-	{
-		std::cout
-		<< "| "
-		<< std::setw(10) << i + 1
-		<< " | "
-		<< std::setw(10) << formatText(_contacts[i].getFirstName())
-		<< " | "
-		<< std::setw(10) << formatText(_contacts[i].getLastName())
-		<< " | "
-		<< std::setw(10) << formatText(_contacts[i].getNickname())
-		<< " |"
-		<< std::endl;
-	}
-	std::cout << "-----------------------------------------------------" << std::endl;
-}
-
-// This function will display the full details of the contact with the specified index.
-void PhoneBook::displayContactByIndex(int index) const
-{
-	int arr_index = index - 1;
-
-	std::cout << std::endl << "Details for contact #" << index << ": " << std::endl;
-	std::cout << std::left
-		<< std::setw(16)
-		<< "First name: " << this->_contacts[arr_index].getFirstName()
-		<< std::endl
-		<< std::setw(16)
-		<< "Last name: " << this->_contacts[arr_index].getLastName()
-		<< std::endl
-		<< std::setw(16)
-		<< "Nickname: " << this->_contacts[arr_index].getNickname()
-		<< std::endl
-		<< std::setw(16)
-		<< "Phone number: " << this->_contacts[arr_index].getPhoneNumber()
-		<< std::endl
-		<< std::setw(16)
-		<< "Darkest secret: " << this->_contacts[arr_index].getDarkestSecret()
-		<< std::endl << std::endl;
-}
 
 // This helper function trims leading and trailing whitespaces from the input.
 static std::string trim(const std::string& input)
@@ -219,4 +136,87 @@ void PhoneBook::searchPhoneBook() const
 			break;
 	}
 	this->displayContactByIndex(index_int);
+}
+
+// This function stores a contact into the phonebook's contacts array and updates
+// the contactCount and oldestIndex variables.
+void PhoneBook::addContact(const Contact &newContact)
+{
+	if (_contactCount < 8)
+	{
+		_contacts[_contactCount] = newContact;
+		_contactCount++;
+	}
+	else
+	{
+		_contacts[_oldestIndex] = newContact;
+		// oldest index will be + 1, but % 8 to remain within the
+		// bounds of the array
+		_oldestIndex = (_oldestIndex + 1) % 8;
+		// contactCount doesn't have to be updated because arr is already
+		// at max contacts
+	}
+}
+
+// This function will truncate the text if it is has more than 10 characters.
+// The first 9 characters will be preserved and appended with a '.'.
+static std::string formatText(const std::string& text)
+{
+	if (text.length() > 10)
+		return text.substr(0, 9) + ".";
+	return text;
+}
+
+// This function displays all the contacts in the phonebook in a summarized form.
+// Workflow:
+// 1) Set the width of each column to 10 characters.
+// setw is a non-persistent manipulator (only applies to the next output) so it should
+// be called last. It will pad shorter text with space on the left (by default).
+// 2) Truncate the text if it exceeds 10 characters, replacing the last
+// character with a dot by calling formatText.
+void PhoneBook::displayContacts() const
+{
+	std::cout << "-----------------------------------------------------" << std::endl;
+	std::cout << "|    Index   | First Name |  Last Name |  Nickname  |" << std::endl;
+	std::cout << "-----------------------------------------------------" << std::endl;
+
+	for (int i = 0; i < _contactCount; i++)
+	{
+		std::cout
+		<< "| "
+		<< std::setw(10) << i + 1
+		<< " | "
+		<< std::setw(10) << formatText(_contacts[i].getFirstName())
+		<< " | "
+		<< std::setw(10) << formatText(_contacts[i].getLastName())
+		<< " | "
+		<< std::setw(10) << formatText(_contacts[i].getNickname())
+		<< " |"
+		<< std::endl;
+	}
+	std::cout << "-----------------------------------------------------" << std::endl;
+}
+
+// This function will display the full details of the contact with the specified index.
+void PhoneBook::displayContactByIndex(int index) const
+{
+	int arr_index = index - 1;
+
+	std::cout << std::endl << "Details for contact #" << index << ": " << std::endl;
+	std::cout << std::left
+		<< std::setw(16)
+		<< "First name: " << this->_contacts[arr_index].getFirstName()
+		<< std::endl
+		<< std::setw(16)
+		<< "Last name: " << this->_contacts[arr_index].getLastName()
+		<< std::endl
+		<< std::setw(16)
+		<< "Nickname: " << this->_contacts[arr_index].getNickname()
+		<< std::endl
+		<< std::setw(16)
+		<< "Phone number: " << this->_contacts[arr_index].getPhoneNumber()
+		<< std::endl
+		<< std::setw(16)
+		<< "Darkest secret: " << this->_contacts[arr_index].getDarkestSecret()
+		<< std::endl << std::endl;
 }
--- 00/ex01/PhoneBook.hpp
+++ 00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 13:36:04 by etien             #+#    #+#             */
-/*   Updated: 2025/02/14 14:55:12 by etien            ###   ########.fr       */
+/*   Updated: 2025/02/17 15:22:55 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,25 @@
 
 class PhoneBook
 {
+	// Internal data members
 	private:
 		Contact _contacts[8];
 		int _contactCount;
 		int _oldestIndex;
 
+	// Constructor, destructor and public methods
 	public:
 		PhoneBook();
 		~PhoneBook();
 
+		void addToPhoneBook();
+		void searchPhoneBook() const;
+
+	// Internal helper functions
+	private:
 		void addContact(const Contact &newContact);
 		void displayContacts() const;
 		void displayContactByIndex(int index) const;
-		void addToPhoneBook();
-		void searchPhoneBook() const;
 };
 
 #endif
