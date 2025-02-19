5cdcbabc5a713b1d28a8b358c88c9971853bd099
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2025-02-19 19:55:07 +0800

    feat: working on 01/ex05.

---
+++ 01/ex05/Harl.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 17:47:29 by etien             #+#    #+#             */
+/*   Updated: 2025/02/19 18:19:19 by etien            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void Harl::debug( void )
+{
+
+}
+
+void Harl::info( void )
+{
+
+}
+
+void Harl::warning( void )
+{
+
+}
+
+void Harl::error( void )
+{
+
+}
+
+Harl::Harl()
+{
+	std::cout << "Harl has been created." << std::endl;
+}
+
+Harl::~Harl()
+{
+	std::cout << "Harl has been destroyed." << std::endl;
+}
+
+void Harl::complain( std::string level )
+{
+
+}
---
+++ 01/ex05/Harl.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 17:47:40 by etien             #+#    #+#             */
+/*   Updated: 2025/02/19 18:15:28 by etien            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+	private:
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+
+	public:
+		Harl();
+		~Harl();
+
+		void complain( std::string level );
+
+};
+
+#endif
