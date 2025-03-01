ef6ebd4c191b0d349a904eff0b0e4a82ff4516f5
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2025-03-01 18:18:58 +0800

    feat: added using ScavTrap::attack in DiamondTrap in 03/ex03. feat: added notes for 03/.

--- 03/ex03/DiamondTrap.hpp
+++ 03/ex03/DiamondTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 17:20:43 by etien             #+#    #+#             */
-/*   Updated: 2025/03/01 15:50:19 by etien            ###   ########.fr       */
+/*   Updated: 2025/03/01 18:04:27 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,12 @@ class DiamondTrap : public ScavTrap, public FragTrap
 		DiamondTrap &operator=(const DiamondTrap &src);
 		~DiamondTrap();
 
+		// If the base class and derived class both have a function of the same name, 
+		// the derived class's method would normally hide the base class' method. 
+		// By using using base_class::function;, you can make the base class' version 
+		// of the function accessible in the derived class.
+		using ScavTrap::attack;
+		
 		void whoAmI();
 };
 
--- 03/ex03/main.cpp
+++ 03/ex03/main.cpp
@@ -6,11 +6,20 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 12:30:02 by etien             #+#    #+#             */
-/*   Updated: 2025/03/01 17:33:43 by etien            ###   ########.fr       */
+/*   Updated: 2025/03/01 18:17:28 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
+
+// This project teaches the precedence of variables and functions 
+// in multiple inheritance.Precedence is typically determined by the 
+// order in which base classes are initialized.
+// Functions and variables with the same name can cause shadowing—always 
+// be mindful of what the local scope refers to. Avoid name conflicts or 
+// resolve them using the scope resolution operator.
+// When a variable is inherited from a base class, only a single instance of 
+// that variable exists, but the derived class can modify it.
 
 int main()
 {
