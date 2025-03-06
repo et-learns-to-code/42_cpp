59308d6976683813358d84a8bd1c5695d9d085de
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2025-03-06 13:57:11 +0800

    fix: added notes for 04/.

--- 04/ex00/main.cpp
+++ 04/ex00/main.cpp
@@ -6,36 +6,40 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 10:56:16 by etien             #+#    #+#             */
-/*   Updated: 2025/03/05 19:00:44 by etien            ###   ########.fr       */
+/*   Updated: 2025/03/06 11:00:00 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
-#include "Dog.hpp"
 #include "Cat.hpp"
-#include "WrongCat.hpp"
+#include "Dog.hpp"
 #include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 // This exercise is designed to introduce you to subtype polymorphism
 // (also known as runtime polymorphism) in C++.
 
 int main()
 {
+	// Test cases from subject
 	{
 		const Animal* meta = new Animal();
 		const Animal* j = new Dog();
 		const Animal* i = new Cat();
 
+		std::cout << meta->getType() << " " << std::endl;
 		std::cout << j->getType() << " " << std::endl;
 		std::cout << i->getType() << " " << std::endl;
 		i->makeSound(); //will output the cat sound!
 		j->makeSound();
 		meta->makeSound();
 	}
+	// Test cases for WrongCat class
 	{
 		const WrongAnimal* meta = new WrongAnimal();
 		const WrongAnimal* i = new WrongCat();
 
+		std::cout << meta->getType() << " " << std::endl;
 		std::cout << i->getType() << " " << std::endl;
 		i->makeSound();
 		meta->makeSound();
