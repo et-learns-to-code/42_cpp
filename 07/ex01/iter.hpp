e2f0360617e268d485e61dbfbc0b170eb43f8bd5
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2025-04-09 13:48:17 +0800

    feat: 07/ex01 - completed and checked.

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 19:37:23 by etien             #+#    #+#             */
-/*   Updated: 2025/04/08 20:14:55 by etien            ###   ########.fr       */
+/*   Updated: 2025/04/09 13:46:42 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define ITER_HPP
 
 #include <cstdlib> // size_t
+#include <iostream>
 
 // A function template iter that takes 3 parameters and returns nothing.
 // The first parameter is the address of an array.
@@ -31,6 +32,13 @@ void iter(T *arr, size_t length, void (*func)(T&))
 	for (size_t i = 0; i < length; i++)
 		// Function pointers are automatically dereferenced when called.
 		func(arr[i]);
+}
+
+// This template will print out the item to the terminal and append a space behind it.
+template <typename T>
+void printItem(T& item)
+{
+	std::cout << item << " ";
 }
 
 #endif
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -6,13 +6,38 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 19:37:26 by etien             #+#    #+#             */
-/*   Updated: 2025/04/08 19:37:57 by etien            ###   ########.fr       */
+/*   Updated: 2025/04/09 13:46:56 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
 int main()
 {
+	std::cout << std::endl;
 
+	std::cout << GREEN << "TEST: INT ARRAY" << RESET << std::endl;
+	int int_arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	iter(int_arr, sizeof(int_arr) / sizeof(int), printItem);
+	std::cout << std::endl << std::endl;
+
+	std::cout << GREEN << "TEST: DOUBLE ARRAY" << RESET << std::endl;
+	double double_arr[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+	iter(double_arr, sizeof(double_arr) / sizeof(double), printItem);
+	std::cout << std::endl << std::endl;
+
+	std::cout << GREEN << "TEST: CHAR ARRAY" << RESET << std::endl;
+	char char_arr[] =  {'a', 'b', 'c', 'd', 'e'};
+	iter(char_arr, sizeof(char_arr) / sizeof(char), printItem);
+	std::cout << std::endl << std::endl;
+
+	std::cout << GREEN << "TEST: STRING OBJECT ARRAY" << RESET << std::endl;
+	std::string string_arr[] = {"this", "is", "a", "string", "object", "array"};
+	iter(string_arr, sizeof(string_arr) / sizeof(std::string), printItem);
+	std::cout << std::endl << std::endl;
+
+	return 0;
 }
