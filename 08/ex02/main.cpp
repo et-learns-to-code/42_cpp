b91eb4bb76e577d2ba45af03426b6dbdd19f15dd
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2025-04-17 19:03:47 +0800

    feat: 08/ex02 - working on main.

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 16:54:41 by etien             #+#    #+#             */
-/*   Updated: 2025/04/17 18:04:42 by etien            ###   ########.fr       */
+/*   Updated: 2025/04/17 19:02:45 by etien            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 #include <iostream>
 #include <list>
+#include <sstream>
 
 #define RED "\033[31m"
 #define GREEN "\033[32m"
@@ -37,80 +38,104 @@
 
 void testSubject()
 {
+	// ostringstream objects store a string buffer.
+	std::ostringstream mutantStackBuffer;
+	std::ostringstream listBuffer;
+	// saving the original cout buffer so we can restore it later.
+	// rdbuf() has two forms:
+	// 1) getter that will return a pointer to a streambuf object
+	// 2) setter that sets the inputted streambuf to be the buffer associated
+	//    with the stream it is called on.
+	//    In this form, it works like dup() in C.
+	std::streambuf *originalBuffer = std::cout.rdbuf();
+
+	// redirecting cout to write to mutantStackBuffer
+	std::cout.rdbuf(mutantStackBuffer.rdbuf());
+	std::cout << YELLOW << "\nTEST: SUBJECT (MUTANTSTACK)" << RESET << std::endl;
+
+	MutantStack<int> mstack;
+
+	// push() - insert element to top of stack.
+	mstack.push(5);
+	mstack.push(17);
+
+	// top() - access next element.
+	std::cout << mstack.top() << std::endl;
+
+	// pop() - remove top element.
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
 	{
-		std::cout << YELLOW << "\nTEST: SUBJECT (MUTANTSTACK)" << RESET << std::endl;
-
-		MutantStack<int> mstack;
-
-		// push() - insert element to top of stack.
-		mstack.push(5);
-		mstack.push(17);
-
-		// top() - access next element.
-		std::cout << mstack.top() << std::endl;
-
-		// pop() - remove top element.
-		mstack.pop();
-
-		std::cout << mstack.size() << std::endl;
-
-		mstack.push(3);
-		mstack.push(5);
-		mstack.push(737);
-		//[...]
-		mstack.push(0);
-
-		MutantStack<int>::iterator it = mstack.begin();
-		MutantStack<int>::iterator ite = mstack.end();
-
+		// print out stack contents
+		// 5, 3, 5, 737, 0
+		std::cout << *it << std::endl;
 		++it;
-		--it;
-		while (it != ite)
-		{
-			// print out stack contents
-			// 5, 3, 5, 737, 0
-			std::cout << *it << std::endl;
-			++it;
-		}
-		std::stack<int> s(mstack);
 	}
-		{
-		std::cout << YELLOW << "\nTEST: SUBJECT (LIST)" << RESET << std::endl;
+	std::stack<int> s(mstack);
 
-		MutantStack<int> mstack;
+	// redirecting cout to write to mutantStackBuffer
+	std::cout.rdbuf(listBuffer.rdbuf());
+	std::cout << YELLOW << "\nTEST: SUBJECT (LIST)" << RESET << std::endl;
 
-		// push() - insert element to top of stack.
-		mstack.push(5);
-		mstack.push(17);
+	std::list<int> list;
 
-		// top() - access next element.
-		std::cout << mstack.top() << std::endl;
+	// push_back() - add element at the end.
+	list.push_back(5);
+	list.push_back(17);
 
-		// pop() - remove top element.
-		mstack.pop();
+	// top() - access next element.
+	std::cout << list.top() << std::endl;
 
-		std::cout << mstack.size() << std::endl;
+	// pop_back() - delete last element.
+	list.pop_back();
 
-		mstack.push(3);
-		mstack.push(5);
-		mstack.push(737);
-		//[...]
-		mstack.push(0);
+	std::cout << list.size() << std::endl;
 
-		MutantStack<int>::iterator it = mstack.begin();
-		MutantStack<int>::iterator ite = mstack.end();
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	//[...]
+	list.push_back(0);
 
+	MutantStack<int>::iterator it = list.begin();
+	MutantStack<int>::iterator ite = list.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		// print out stack contents
+		// 5, 3, 5, 737, 0
+		std::cout << *it << std::endl;
 		++it;
-		--it;
-		while (it != ite)
-		{
-			// print out stack contents
-			// 5, 3, 5, 737, 0
-			std::cout << *it << std::endl;
-			++it;
-		}
-		std::stack<int> s(mstack);
 	}
+	std::stack<int> l(list);
+
+	std::cout << YELLOW << "\nRESULT OF COMPARISON: " << RESET << std::endl;
+
+	std::cout.rdbuf(originalBuffer);
+
+	std::cout << "mutantStack output: " << mutantStackBuffer.str() << std::endl;
+	std::cout << "List output: " << listBuffer.str() << std::endl;
+
+	if (mutantStackBuffer.str() == listBuffer.str())
+		std::cout << GREEN << "Output from mutantStack and list are identical." << RESET << std::endl;
+	else
+		std::cout << RED << "Output from mutantStack and list differ." << RESET << std::endl;
 }
 
 int main()
